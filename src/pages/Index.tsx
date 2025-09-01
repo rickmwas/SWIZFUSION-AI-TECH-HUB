d798c9b5dcea07cc439dad0ed6e9866cf6bbf16a
rickmwas <rickmwasswiz@gmail.com>
2025-09-01 11:31:34 +0300

    Add Mailchimp popup script for email form on scroll

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import About from '../components/About';
@@ -11,6 +11,21 @@ import Testimonials from '../components/Testimonials';
 import FAQ from '../components/FAQ';
 
 export default function Index() {
+  useEffect(() => {
+    const handleScroll = () => {
+      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 2) {
+        if (!document.getElementById('mcjs')) {
+          const script = document.createElement('script');
+          script.id = 'mcjs';
+          script.async = true;
+          script.src = 'https://chimpstatic.com/mcjs-connected/js/users/2bff5011ab0f0afa69552f7a8/02a4072ee0e339890111721ec.js';
+          document.body.appendChild(script);
+        }
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
   return (
     <div className="min-h-screen">
       <Navbar />
@@ -20,9 +35,9 @@ export default function Index() {
         <Services />
         <Products />
         <Pricing />
-  <Testimonials />
-  <FAQ />
-  <ContactForm />
+        <Testimonials />
+        <FAQ />
+        <ContactForm />
       </main>
       <Footer />
     </div>
